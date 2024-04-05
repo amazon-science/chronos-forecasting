2050ecf84707589b0641030228b9902dc298ac5f
Abdul Fatir Ansari <ansarnd@amazon.com>
2024-04-05 17:11:12 +0200

    Add test for top_p

--- test/test_chronos_mlx.py
+++ test/test_chronos_mlx.py
@@ -4,9 +4,11 @@
 from pathlib import Path
 from typing import Tuple
 
+import mlx.core as mx
 import numpy as np
 import pytest
 
+from chronos.mlx.t5 import apply_top_p
 from chronos.mlx import ChronosPipeline
 
 
@@ -95,3 +97,58 @@ def test_pipeline_embed(dtype: str):
     embedding, scale = pipeline.embed(context[0, ...])
     validate_array(embedding, (1, expected_embed_length, d_model))
     validate_array(scale, (1,))
+
+
+@pytest.mark.parametrize(
+    "top_p,expected_non_zero_probs",
+    [
+        (
+            0.1,
+            mx.array(
+                [
+                    [False, True, False, False],
+                    [False, True, False, False],
+                    [True, False, False, False],
+                    [True, False, False, False],
+                    [False, False, False, True],
+                ]
+            ),
+        ),
+        (
+            0.5,
+            mx.array(
+                [
+                    [False, True, False, False],
+                    [False, True, False, False],
+                    [True, False, False, False],
+                    [True, False, False, False],
+                    [False, False, True, True],
+                ]
+            ),
+        ),
+        (
+            0.95,
+            mx.array(
+                [
+                    [False, True, True, True],
+                    [False, True, False, True],
+                    [True, False, False, False],
+                    [True, True, False, False],
+                    [False, True, True, True],
+                ]
+            ),
+        ),
+    ],
+)
+def test_apply_top_p(top_p: float, expected_non_zero_probs: mx.array):
+    probs = mx.array(
+        [
+            [0.1, 0.4, 0.3, 0.2],
+            [0.01, 0.39, 0.25, 0.35],
+            [0.9, 0.01, 0.01, 0.08],
+            [0.7, 0.2, 0.05, 0.05],
+            [0.25, 0.25, 0.25, 0.25],
+        ],
+    )
+    top_p_probs = mx.softmax(apply_top_p(probs.log(), top_p=top_p), axis=-1)
+    assert mx.all(mx.not_equal(top_p_probs, 0.0) == expected_non_zero_probs)
