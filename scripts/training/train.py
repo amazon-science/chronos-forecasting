ebdb329f56e2aa782a592d8c5422642bf4414bff
Andrei Chernov <chernov.andrey.998@gmail.com>
2024-10-27 19:30:20 +0100

    Added the validation dataset to the training/finetuning script

--- scripts/training/train.py
+++ scripts/training/train.py
@@ -45,9 +45,21 @@ from gluonts.transform import (
 )
 
 from chronos import ChronosConfig, ChronosTokenizer
-
+from sklearn.metrics import mean_absolute_error
 
 app = typer.Typer(pretty_exceptions_enable=False)
+
+
+def compute_metrics(pred):
+    labels = pred.label_ids.astype(np.float32)
+    mae = mean_absolute_error(labels, pred.predictions[0].argmax(-1).astype(np.float32))
+    # MAE should be scaled to represent the distance.
+    # For validation, this scaling does not affect the result, as the argmin remains the same.
+    return {
+        # 'MAE': 30.0 * mae / 4096,
+        'MAE': mae,
+
+    }
 
 
 def is_main_process() -> bool:
@@ -118,10 +130,10 @@ def save_training_info(ckpt_path: Path, training_config: Dict):
 
 
 def get_next_path(
-    base_fname: str,
-    base_dir: Path,
-    file_type: str = "yaml",
-    separator: str = "-",
+        base_fname: str,
+        base_dir: Path,
+        file_type: str = "yaml",
+        separator: str = "-",
 ):
     """
     Gets the next available path in a directory. For example, if `base_fname="results"`
@@ -153,13 +165,13 @@ def get_next_path(
 
 
 def load_model(
-    model_id="google/t5-efficient-tiny",
-    model_type="seq2seq",
-    vocab_size=4096,
-    random_init=False,
-    tie_embeddings=False,
-    pad_token_id=0,
-    eos_token_id=1,
+        model_id="google/t5-efficient-tiny",
+        model_type="seq2seq",
+        vocab_size=4096,
+        random_init=False,
+        tie_embeddings=False,
+        pad_token_id=0,
+        eos_token_id=1,
 ):
     """
     Load the specified HuggingFace model, adjusting the vocabulary
@@ -193,7 +205,7 @@ def load_model(
 
 
 def has_enough_observations(
-    entry: dict, min_length: int = 0, max_missing_prop: float = 1.0
+        entry: dict, min_length: int = 0, max_missing_prop: float = 1.0
 ) -> bool:
     """
     Check if the given entry has enough observations in the ``"target"`` attribute.
@@ -209,8 +221,8 @@ def has_enough_observations(
         attribute.
     """
     if (
-        len(entry["target"]) >= min_length
-        and np.isnan(entry["target"]).mean() <= max_missing_prop
+            len(entry["target"]) >= min_length
+            and np.isnan(entry["target"]).mean() <= max_missing_prop
     ):
         return True
     return False
@@ -296,18 +308,18 @@ class ChronosDataset(IterableDataset, ShuffleMixin):
     """
 
     def __init__(
-        self,
-        datasets: list,
-        probabilities: List[float],
-        tokenizer: ChronosTokenizer,
-        context_length: int = 512,
-        prediction_length: int = 64,
-        drop_prob: float = 0.2,
-        min_past: Optional[int] = None,
-        model_type: str = "seq2seq",
-        imputation_method: Optional[MissingValueImputation] = None,
-        mode: str = "training",
-        np_dtype=np.float32,
+            self,
+            datasets: list,
+            probabilities: List[float],
+            tokenizer: ChronosTokenizer,
+            context_length: int = 512,
+            prediction_length: int = 64,
+            drop_prob: float = 0.2,
+            min_past: Optional[int] = None,
+            model_type: str = "seq2seq",
+            imputation_method: Optional[MissingValueImputation] = None,
+            mode: str = "training",
+            np_dtype=np.float32,
     ) -> None:
         super().__init__()
 
@@ -502,45 +514,47 @@ class ChronosDataset(IterableDataset, ShuffleMixin):
 @app.command()
 @use_yaml_config(param_name="config")
 def main(
-    training_data_paths: str,
-    probability: Optional[str] = None,
-    context_length: int = 512,
-    prediction_length: int = 64,
-    min_past: int = 64,
-    max_steps: int = 200_000,
-    save_steps: int = 50_000,
-    log_steps: int = 500,
-    per_device_train_batch_size: int = 32,
-    learning_rate: float = 1e-3,
-    optim: str = "adamw_torch_fused",
-    shuffle_buffer_length: int = 100,
-    gradient_accumulation_steps: int = 2,
-    model_id: str = "google/t5-efficient-tiny",
-    model_type: str = "seq2seq",
-    random_init: bool = False,
-    tie_embeddings: bool = False,
-    output_dir: str = "./output/",
-    tf32: bool = True,
-    torch_compile: bool = True,
-    tokenizer_class: str = "MeanScaleUniformBins",
-    tokenizer_kwargs: str = "{'low_limit': -15.0, 'high_limit': 15.0}",
-    n_tokens: int = 4096,
-    n_special_tokens: int = 2,
-    pad_token_id: int = 0,
-    eos_token_id: int = 1,
-    use_eos_token: bool = True,
-    lr_scheduler_type: str = "linear",
-    warmup_ratio: float = 0.0,
-    dataloader_num_workers: int = 1,
-    max_missing_prop: float = 0.9,
-    num_samples: int = 20,
-    temperature: float = 1.0,
-    top_k: int = 50,
-    top_p: float = 1.0,
-    seed: Optional[int] = None,
+        training_data_paths: str,
+        probability: Optional[str] = None,
+        context_length: int = 512,
+        prediction_length: int = 64,
+        min_past: int = 64,
+        max_steps: int = 200_000,
+        save_steps: int = 50_000,
+        log_steps: int = 500,
+        per_device_train_batch_size: int = 32,
+        learning_rate: float = 1e-3,
+        optim: str = "adamw_torch_fused",
+        shuffle_buffer_length: int = 100,
+        gradient_accumulation_steps: int = 2,
+        model_id: str = "google/t5-efficient-tiny",
+        model_type: str = "seq2seq",
+        random_init: bool = False,
+        tie_embeddings: bool = False,
+        output_dir: str = "./output/",
+        tf32: bool = True,
+        torch_compile: bool = True,
+        tokenizer_class: str = "MeanScaleUniformBins",
+        tokenizer_kwargs: str = "{'low_limit': -15.0, 'high_limit': 15.0}",
+        n_tokens: int = 4096,
+        n_special_tokens: int = 2,
+        pad_token_id: int = 0,
+        eos_token_id: int = 1,
+        use_eos_token: bool = True,
+        lr_scheduler_type: str = "linear",
+        warmup_ratio: float = 0.0,
+        dataloader_num_workers: int = 1,
+        max_missing_prop: float = 0.9,
+        num_samples: int = 20,
+        temperature: float = 1.0,
+        top_k: int = 50,
+        top_p: float = 1.0,
+        seed: Optional[int] = None,
+        use_kernel_softmax: bool = False,
+        validation_data_paths=typer.Option(None, "--validation-data-paths", "-t")
 ):
     if tf32 and not (
-        torch.cuda.is_available() and torch.cuda.get_device_capability()[0] >= 8
+            torch.cuda.is_available() and torch.cuda.get_device_capability()[0] >= 8
     ):
         # TF32 floating point format is available only on NVIDIA GPUs
         # with compute capability 8 and above. See link for details.
@@ -553,7 +567,7 @@ def main(
         tf32 = False
 
     if seed is None:
-        seed = random.randint(0, 2**32)
+        seed = random.randint(0, 2 ** 32)
 
     log_on_main(f"Using SEED: {seed}", logger)
     transformers.set_seed(seed=seed)
@@ -562,6 +576,10 @@ def main(
     output_dir = Path(output_dir)
     training_data_paths = ast.literal_eval(training_data_paths)
     assert isinstance(training_data_paths, list)
+
+    if validation_data_paths:
+        validation_data_paths = ast.literal_eval(validation_data_paths)
+        assert isinstance(validation_data_paths, list)
 
     if isinstance(probability, str):
         probability = ast.literal_eval(probability)
@@ -611,6 +629,19 @@ def main(
         for data_path in training_data_paths
     ]
 
+    if validation_data_paths:
+        val_datasets = [
+            Filter(
+                partial(
+                    has_enough_observations,
+                    min_length=0,
+                    max_missing_prop=0.1,
+                ),
+                FileDataset(path=Path(data_path), freq="h"),
+            )
+            for data_path in validation_data_paths
+        ]
+
     log_on_main("Initializing model", logger)
 
     model = load_model(
@@ -655,34 +686,60 @@ def main(
         mode="training",
     ).shuffle(shuffle_buffer_length=shuffle_buffer_length)
 
-    # Define training args
-    training_args = TrainingArguments(
-        output_dir=str(output_dir),
-        per_device_train_batch_size=per_device_train_batch_size,
-        learning_rate=learning_rate,
-        lr_scheduler_type=lr_scheduler_type,
-        warmup_ratio=warmup_ratio,
-        optim=optim,
-        logging_dir=str(output_dir / "logs"),
-        logging_strategy="steps",
-        logging_steps=log_steps,
-        save_strategy="steps",
-        save_steps=save_steps,
-        report_to=["tensorboard"],
-        max_steps=max_steps,
-        gradient_accumulation_steps=gradient_accumulation_steps,
-        dataloader_num_workers=dataloader_num_workers,
-        tf32=tf32,  # remove this if not using Ampere GPUs (e.g., A100)
-        torch_compile=torch_compile,
-        ddp_find_unused_parameters=False,
-        remove_unused_columns=False,
-    )
+    # Define training arguments
+    base_training_args = {
+        "output_dir": str(output_dir),
+        "per_device_train_batch_size": per_device_train_batch_size,
+        "learning_rate": learning_rate,
+        "lr_scheduler_type": lr_scheduler_type,
+        "warmup_ratio": warmup_ratio,
+        "optim": optim,
+        "logging_dir": str(output_dir / "logs"),
+        "logging_strategy": "steps",
+        "logging_steps": log_steps,
+        "save_strategy": "steps",
+        "save_steps": save_steps,
+        "report_to": ["tensorboard"],
+        "max_steps": max_steps,
+        "gradient_accumulation_steps": gradient_accumulation_steps,
+        "dataloader_num_workers": dataloader_num_workers,
+        "tf32": tf32,  # remove this if not using Ampere GPUs (e.g., A100)
+        "torch_compile": torch_compile,
+        "ddp_find_unused_parameters": False,
+        "remove_unused_columns": False,
+    }
+
+    if validation_data_paths:
+        validation_dataset = ChronosDataset(
+            datasets=val_datasets,
+            probabilities=probability,
+            tokenizer=chronos_config.create_tokenizer(),
+            context_length=context_length,
+            prediction_length=prediction_length,
+            min_past=prediction_length,
+            model_type=model_type,
+            imputation_method=LastValueImputation() if model_type == "causal" else None,
+            mode="validation",
+        )
+        # Add evaluation-related arguments only if validation dataset exists
+        training_args = TrainingArguments(
+            **base_training_args,
+            evaluation_strategy="steps",
+            eval_steps=save_steps,
+            metric_for_best_model="MAE",
+            greater_is_better=False,
+        )
+    else:
+        validation_dataset = None
+        training_args = TrainingArguments(**base_training_args)
 
     # Create Trainer instance
-    trainer = Trainer(
+    trainer = Trainer(  # Trainer in original code
         model=model,
         args=training_args,
         train_dataset=shuffled_train_dataset,
+        eval_dataset=validation_dataset,
+        compute_metrics=compute_metrics
     )
     log_on_main("Training", logger)
 
