15600f19e7b5710a57030d98bbd72a07311bdbbb
Lorenzo Stella <stellalo@amazon.com>
2024-12-04 16:17:34 +0100

    black

--- test/test_utils.py
+++ test/test_utils.py
@@ -4,10 +4,13 @@ import torch
 from chronos.utils import left_pad_and_stack_1D
 
 
-@pytest.mark.parametrize("tensors", [
-    list(map(torch.tensor, [[1, 2, 3], [5, 6]])),
-    list(map(torch.tensor, [[2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0, 10.0]])),
-])
+@pytest.mark.parametrize(
+    "tensors",
+    [
+        list(map(torch.tensor, [[1, 2, 3], [5, 6]])),
+        list(map(torch.tensor, [[2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0, 10.0]])),
+    ],
+)
 def test_pad_and_stack(tensors: list):
     stacked_and_padded = left_pad_and_stack_1D(tensors)
     assert stacked_and_padded.shape == (len(tensors), max(len(t) for t in tensors))
