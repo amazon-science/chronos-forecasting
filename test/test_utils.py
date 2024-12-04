662415b5beb1919d5bcac2642c100a67f3231135
Lorenzo Stella <stellalo@amazon.com>
2024-12-04 16:11:12 +0100

    add assertion

--- test/test_utils.py
+++ test/test_utils.py
@@ -14,4 +14,5 @@ def test_pad_and_stack(tensors: list):
 
     ref = torch.concat(tensors).to(dtype=stacked_and_padded.dtype)
 
+    assert torch.sum(stacked_and_padded) == torch.sum(ref)
     assert torch.nanmean(stacked_and_padded) == torch.nanmean(ref)
