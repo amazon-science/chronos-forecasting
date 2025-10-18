cb7511dfe2ec011541f861aa316a845dc10d7f3d
Abdul Fatir <abdulfatirs@gmail.com>
2025-10-18 12:13:15 +0000

    Generalize eval script

--- scripts/evaluation/evaluate.py
+++ scripts/evaluation/evaluate.py
@@ -15,22 +15,13 @@ from gluonts.model.evaluation import evaluate_forecasts
 from gluonts.model.forecast import QuantileForecast, SampleForecast
 from tqdm.auto import tqdm
 
-from chronos import (
-    BaseChronosPipeline,
-    ChronosBoltPipeline,
-    ChronosPipeline,
-    ForecastType,
-)
+from chronos import BaseChronosPipeline, Chronos2Pipeline, ChronosBoltPipeline, ChronosPipeline, ForecastType
 
 app = typer.Typer(pretty_exceptions_enable=False)
 
 
 def to_gluonts_univariate(hf_dataset: datasets.Dataset):
-    series_fields = [
-        col
-        for col in hf_dataset.features
-        if isinstance(hf_dataset.features[col], datasets.Sequence)
-    ]
+    series_fields = [col for col in hf_dataset.features if isinstance(hf_dataset.features[col], datasets.Sequence)]
     series_fields.remove("timestamp")
     dataset_length = hf_dataset.info.splits["train"].num_examples * len(series_fields)
 
@@ -65,9 +56,7 @@ def load_and_split_dataset(backtest_config: dict):
     # be distribued due to license restrictions and must be generated on the fly
     trust_remote_code = True if hf_repo == "autogluon/chronos_datasets_extra" else False
 
-    ds = datasets.load_dataset(
-        hf_repo, dataset_name, split="train", trust_remote_code=trust_remote_code
-    )
+    ds = datasets.load_dataset(hf_repo, dataset_name, split="train", trust_remote_code=trust_remote_code)
     ds.set_format("numpy")
 
     gts_dataset = to_gluonts_univariate(ds)
@@ -105,9 +94,7 @@ def generate_forecasts(
         forecast_start_date = ts["start"] + len(ts["target"])
 
         if pipeline.forecast_type == ForecastType.SAMPLES:
-            forecasts.append(
-                SampleForecast(samples=item, start_date=forecast_start_date)
-            )
+            forecasts.append(SampleForecast(samples=item, start_date=forecast_start_date))
         elif pipeline.forecast_type == ForecastType.QUANTILES:
             forecasts.append(
                 QuantileForecast(
@@ -120,78 +107,14 @@ def generate_forecasts(
     return forecasts
 
 
-@app.command()
-def main(
+def eval_pipeline_and_save_results(
+    pipeline: BaseChronosPipeline,
     config_path: Path,
     metrics_path: Path,
-    chronos_model_id: str = "amazon/chronos-t5-small",
-    device: str = "cuda",
-    torch_dtype: str = "bfloat16",
-    batch_size: int = 32,
-    num_samples: int = 20,
-    temperature: Optional[float] = None,
-    top_k: Optional[int] = None,
-    top_p: Optional[float] = None,
+    chronos_model_id: str,
+    batch_size: int,
+    **predict_kwargs,
 ):
-    """Evaluate Chronos models.
-
-    Parameters
-    ----------
-    config_path : Path
-        Path to the evaluation config. See ./configs/.
-    metrics_path : Path
-        Path to the CSV file where metrics will be saved.
-    chronos_model_id : str, optional, default = "amazon/chronos-t5-small"
-        HuggingFace ID of the Chronos model or local path
-        Available models on HuggingFace:
-        Chronos:
-            - amazon/chronos-t5-tiny
-            - amazon/chronos-t5-mini
-            - amazon/chronos-t5-small
-            - amazon/chronos-t5-base
-            - amazon/chronos-t5-large
-        Chronos-Bolt:
-            - amazon/chronos-bolt-tiny
-            - amazon/chronos-bolt-mini
-            - amazon/chronos-bolt-small
-            - amazon/chronos-bolt-base
-    device : str, optional, default = "cuda"
-        Device on which inference will be performed
-    torch_dtype : str, optional
-        Model's dtype, by default "bfloat16"
-    batch_size : int, optional, default = 32
-        Batch size for inference. For Chronos-Bolt models, significantly larger
-        batch sizes can be used
-    num_samples : int, optional, default = 20
-        Number of samples to draw when using the original Chronos models
-    temperature : Optional[float], optional, default = 1.0
-        Softmax temperature to used for the original Chronos models
-    top_k : Optional[int], optional, default = 50
-        Top-K sampling, by default None
-    top_p : Optional[float], optional, default = 1.0
-        Top-p sampling, by default None
-    """
-    if isinstance(torch_dtype, str):
-        torch_dtype = getattr(torch, torch_dtype)
-    assert isinstance(torch_dtype, torch.dtype)
-
-    # Load Chronos
-    pipeline = BaseChronosPipeline.from_pretrained(
-        chronos_model_id,
-        device_map=device,
-        torch_dtype=torch_dtype,
-    )
-
-    if isinstance(pipeline, ChronosPipeline):
-        predict_kwargs = dict(
-            num_samples=num_samples,
-            temperature=temperature,
-            top_k=top_k,
-            top_p=top_p,
-        )
-    elif isinstance(pipeline, ChronosBoltPipeline):
-        predict_kwargs = {}
-
     # Load backtest configs
     with open(config_path) as fp:
         backtest_configs = yaml.safe_load(fp)
@@ -204,10 +127,7 @@ def main(
         logger.info(f"Loading {dataset_name}")
         test_data = load_and_split_dataset(backtest_config=config)
 
-        logger.info(
-            f"Generating forecasts for {dataset_name} "
-            f"({len(test_data.input)} time series)"
-        )
+        logger.info(f"Generating forecasts for {dataset_name} ({len(test_data.input)} time series)")
         forecasts = generate_forecasts(
             test_data.input,
             pipeline=pipeline,
@@ -230,9 +150,7 @@ def main(
             .reset_index(drop=True)
             .to_dict(orient="records")
         )
-        result_rows.append(
-            {"dataset": dataset_name, "model": chronos_model_id, **metrics[0]}
-        )
+        result_rows.append({"dataset": dataset_name, "model": chronos_model_id, **metrics[0]})
 
     # Save results to a CSV file
     results_df = (
@@ -244,6 +162,175 @@ def main(
         .sort_values(by="dataset")
     )
     results_df.to_csv(metrics_path, index=False)
+
+
+@app.command()
+def chronos(
+    config_path: Path,
+    metrics_path: Path,
+    chronos_model_id: str = "amazon/chronos-t5-small",
+    device: str = "cuda",
+    torch_dtype: str = "bfloat16",
+    batch_size: int = 32,
+    num_samples: int = 20,
+    temperature: Optional[float] = None,
+    top_k: Optional[int] = None,
+    top_p: Optional[float] = None,
+):
+    """Evaluate Chronos models.
+
+    Parameters
+    ----------
+    config_path : Path
+        Path to the evaluation config. See ./configs/.
+    metrics_path : Path
+        Path to the CSV file where metrics will be saved.
+    chronos_model_id : str, optional, default = "amazon/chronos-t5-small"
+        HuggingFace ID of the Chronos model or local path
+        Available models IDs:
+            - amazon/chronos-t5-tiny
+            - amazon/chronos-t5-mini
+            - amazon/chronos-t5-small
+            - amazon/chronos-t5-base
+            - amazon/chronos-t5-large
+    device : str, optional, default = "cuda"
+        Device on which inference will be performed
+    torch_dtype : str, optional
+        Model's dtype, by default "bfloat16"
+    batch_size : int, optional, default = 32
+        Batch size for inference. For Chronos-Bolt models, significantly larger
+        batch sizes can be used
+    num_samples : int, optional, default = 20
+        Number of samples to draw when using the original Chronos models
+    temperature : Optional[float], optional, default = 1.0
+        Softmax temperature to used for the original Chronos models
+    top_k : Optional[int], optional, default = 50
+        Top-K sampling, by default None
+    top_p : Optional[float], optional, default = 1.0
+        Top-p sampling, by default None
+    """
+    if isinstance(torch_dtype, str):
+        torch_dtype = getattr(torch, torch_dtype)
+    assert isinstance(torch_dtype, torch.dtype)
+
+    # Load Chronos
+    pipeline = BaseChronosPipeline.from_pretrained(chronos_model_id, device_map=device, torch_dtype=torch_dtype)
+
+    assert isinstance(pipeline, ChronosPipeline)
+
+    eval_pipeline_and_save_results(
+        pipeline=pipeline,
+        config_path=config_path,
+        metrics_path=metrics_path,
+        chronos_model_id=chronos_model_id,
+        batch_size=batch_size,
+        num_samples=num_samples,
+        temperature=temperature,
+        top_k=top_k,
+        top_p=top_p,
+    )
+
+
+@app.command()
+def chronos_bolt(
+    config_path: Path,
+    metrics_path: Path,
+    chronos_model_id: str = "amazon/chronos-bolt-base",
+    device: str = "cuda",
+    torch_dtype: str = "float32",
+    batch_size: int = 32,
+):
+    """Evaluate Chronos-Bolt models.
+
+    Parameters
+    ----------
+    config_path : Path
+        Path to the evaluation config. See ./configs/.
+    metrics_path : Path
+        Path to the CSV file where metrics will be saved.
+    chronos_model_id : str, optional, default = "amazon/chronos-t5-small"
+        HuggingFace ID of the Chronos model or local path
+        Available model IDs:
+            - amazon/chronos-bolt-tiny
+            - amazon/chronos-bolt-mini
+            - amazon/chronos-bolt-small
+            - amazon/chronos-bolt-base
+    device : str, optional, default = "cuda"
+        Device on which inference will be performed
+    torch_dtype : str, optional
+        Model's dtype, by default "bfloat16"
+    batch_size : int, optional, default = 32
+        Batch size for inference. For Chronos-Bolt models, significantly larger
+        batch sizes can be used
+    """
+    if isinstance(torch_dtype, str):
+        torch_dtype = getattr(torch, torch_dtype)
+    assert isinstance(torch_dtype, torch.dtype)
+
+    # Load Chronos
+    pipeline = BaseChronosPipeline.from_pretrained(chronos_model_id, device_map=device, torch_dtype=torch_dtype)
+
+    assert isinstance(pipeline, ChronosBoltPipeline)
+
+    eval_pipeline_and_save_results(
+        pipeline=pipeline,
+        config_path=config_path,
+        metrics_path=metrics_path,
+        chronos_model_id=chronos_model_id,
+        batch_size=batch_size,
+    )
+
+
+@app.command()
+def chronos2(
+    config_path: Path,
+    metrics_path: Path,
+    chronos_model_id: str = "amazon/chronos-2",
+    device: str = "cuda",
+    torch_dtype: str = "float32",
+    batch_size: int = 32,
+    predict_batches_jointly: bool = False,
+):
+    """Evaluate Chronos-Bolt models.
+
+    Parameters
+    ----------
+    config_path : Path
+        Path to the evaluation config. See ./configs/.
+    metrics_path : Path
+        Path to the CSV file where metrics will be saved.
+    chronos_model_id : str, optional, default = "amazon/chronos-t5-small"
+        HuggingFace ID of the Chronos model or local path
+        Available model IDs:
+            - amazon/chronos-2
+    device : str, optional, default = "cuda"
+        Device on which inference will be performed
+    torch_dtype : str, optional
+        Model's dtype, by default "bfloat16"
+    batch_size : int, optional, default = 32
+        Batch size for inference. For Chronos-Bolt models, significantly larger
+        batch sizes can be used
+    predict_batches_jointly: bool, optional, default = False
+        If True, cross-learning is enables and model makes joint predictions for all
+        items in the batch
+    """
+    if isinstance(torch_dtype, str):
+        torch_dtype = getattr(torch, torch_dtype)
+    assert isinstance(torch_dtype, torch.dtype)
+
+    # Load Chronos
+    pipeline = BaseChronosPipeline.from_pretrained(chronos_model_id, device_map=device, torch_dtype=torch_dtype)
+
+    assert isinstance(pipeline, Chronos2Pipeline)
+
+    eval_pipeline_and_save_results(
+        pipeline=pipeline,
+        config_path=config_path,
+        metrics_path=metrics_path,
+        chronos_model_id=chronos_model_id,
+        batch_size=batch_size,
+        predict_batches_jointly=predict_batches_jointly,
+    )
 
 
 if __name__ == "__main__":
