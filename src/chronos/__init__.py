0b9f336cd00332a2f6d1f7d4609ccaf7233e1f91
Abdul Fatir <abdulfatirs@gmail.com>
2025-10-19 02:59:08 +0000

    Use dynamic versioning and bump version

--- src/chronos/__init__.py
+++ src/chronos/__init__.py
@@ -1,6 +1,7 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
+from .__about__ import __version__
 from .base import BaseChronosPipeline, ForecastType
 from .chronos import (
     ChronosConfig,
@@ -13,6 +14,7 @@ from .chronos2 import Chronos2ForecastingConfig, Chronos2Model, Chronos2Pipeline
 from .chronos_bolt import ChronosBoltConfig, ChronosBoltPipeline
 
 __all__ = [
+    "__version__",
     "BaseChronosPipeline",
     "ForecastType",
     "ChronosConfig",
